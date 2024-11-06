6e172905220ac3a32e5b4332c9d6a99aff50e3c5
agrim08 <agrimgupta0805@gmail.com>
2024-11-07 00:22:45 +0530

    changed db name

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://agrimgupta8105:agrimgupta8105@agrim.yxnkx.mongodb.net/namasteNode"
+    "mongodb+srv://agrimgupta8105:agrimgupta8105@agrim.yxnkx.mongodb.net/DevMate"
   );
 };
 
