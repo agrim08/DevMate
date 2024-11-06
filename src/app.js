110e9da0e6bf678c54f8136491759b44e6c3a77b
agrim08 <agrimgupta0805@gmail.com>
2024-11-06 19:15:28 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,45 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({
-    name: "Agrim",
-    age: 19,
-  });
-});
-
-app.post("/user", (req, res) => {
-  console.log("saved the data");
-  res.send("Data saved successfully");
-});
-
-app.delete("/user", (req, res) => {
-  console.log("deleted successfully");
-  res.send("deleted successfully");
-});
-
-app.patch("/user", (req, res) => {
-  res.send({
-    role: "student",
-  });
-});
-
-//reading user info:
-app.get("/testuser/:userID", (req, res) => {
-  console.log(req.params);
-  console.log(req.query); //printed to console
-  res.send({ city: "delhi", country: "India" });
-});
-
-//we can write regex also:
-app.get(/a/, (req, res) => {
-  res.send("Testing regex");
-});
-
-//This is for all http methods
-app.use("/test", (req, res) => {
-  res.send("Test");
-});
+app.use("/user", [
+  //we can send array of route handlers
+  (req, res, next) => {
+    console.log("hello");
+    // res.send("hello from 1st route handler");
+    next();
+  },
+  (req, res, next) => {
+    console.log("hello 2nd");
+    // res.send("hello from 2nd route handler");
+    next();
+  },
+  (req, res) => {
+    console.log("hello 3rd");
+    res.send("hello from 3rd route handler");
+  },
+]);
 
 app.listen(4000, () => {
   console.log("listening on port 4000");
