788ef36581a70097def13206b8aaafb7331ff6f9
agrim08 <agrimgupta0805@gmail.com>
2024-12-17 21:05:22 +0530

    changes

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// Load environment variables
+dotenv.config();
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://agrimgupta8105:agrimgupta8105@agrim.yxnkx.mongodb.net/DevMate"
-  );
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected...");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1); // Exit process with failure
+  }
 };
 
 module.exports = connectDB;
