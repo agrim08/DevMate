7c82da54cc0924e9e7f709be13c6ecb6eb558c48
agrim08 <agrimgupta0805@gmail.com>
2024-12-20 20:49:59 +0530

    setup routing and build footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./index.css";
+import Login from "./components/Login";
+import Body from "./components/Body";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <div>Hii</div>;
+};
+
+export default Login;
