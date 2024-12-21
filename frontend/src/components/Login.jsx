4e03e3629119467aeebaecabe96061687b071cd6
agrim08 <agrimgupta0805@gmail.com>
2024-12-21 23:32:29 +0530

    refactoring

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
 import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
   const [emailId, setEmailId] = useState("");
@@ -14,7 +15,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "http://localhost:4000/login",
+        `${BASE_URL}/login`,
         {
           emailId: emailId,
           password: password,
