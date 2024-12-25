2e3c425b81072b11c68798d4b08d03387cb1ce8e
agrim08 <agrimgupta0805@gmail.com>
2024-12-25 21:08:55 +0530

    bug fixed

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -10,7 +10,6 @@ const Feed = () => {
   const feed = useSelector((store) => store.feed);
 
   const getFeed = async () => {
-    if (feed) return;
     try {
       const res = await axios.get(`${BASE_URL}/feed`, {
         withCredentials: true,
