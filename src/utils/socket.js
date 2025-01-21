c60742370b29f5de573d6d690757f382523f456d
agrim08 <agrimgupta0805@gmail.com>
2025-01-21 21:04:52 +0530

    chat functionaiity

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,17 +1,34 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+
+const getRoomId = (userId, targetUserId) => {
+  return crypto
+    .createHash("sha256")
+    .update([userId, targetUserId].sort().join("_"))
+    .digest("hex");
+};
 
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
-      origin: "http://localhost:5173/",
+      origin: "http://localhost:5173",
       methods: ["GET", "POST", "PUT", "PATCH"],
     },
   });
 
   io.on("connection", (socket) => {
-    socket.on("joinChat", {});
-    socket.on("sendMessage", {});
-    socket.on("disconnect", {});
+    socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+      const room = getRoomId(userId, targetUserId);
+      console.log(`${firstName} joined room ${room}`);
+      socket.join(room);
+    });
+
+    socket.on("sendMessage", ({ firstName, userId, targetUserId, content }) => {
+      const room = getRoomId(userId, targetUserId);
+      console.log(`${firstName} ${content}`);
+      io.to(room).emit("messageRecieved", { firstName, content });
+    });
+    socket.on("disconnect", () => {});
   });
 };
 
